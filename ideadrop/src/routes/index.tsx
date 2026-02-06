b470d5ba8e0afd339d9e5e57a77f3799451761d3
Alex Bilba <alex.bilba@mybuildcore.com>
2026-02-06 11:14:15 -0800

    setup

---
+++ ideadrop/src/routes/index.tsx
@@ -0,0 +1,8 @@
+import { createFileRoute } from '@tanstack/react-router';
+import '../App.css';
+
+export const Route = createFileRoute('/')({ component: App });
+
+function App() {
+	return <>My app</>;
+}
